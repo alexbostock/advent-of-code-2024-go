e4531899e7d31f33a8f188dfcfdb61f86b00135d
Alex Bostock <alex@bostock.uk>
2024-12-08 09:41:26 +0000

    Solve day 2

--- main.go
+++ main.go
@@ -12,11 +12,17 @@ import (
 )
 
 func main() {
+	puzzle1()
+	puzzle2()
+}
+
+func puzzle1() {
+	fmt.Println("Puzzle 1")
 	input, err := os.Open("./input/1.txt")
 	if err != nil {
 		panic(err)
 	}
-	left, right, err := ParseInput(input)
+	left, right, err := ParseInput1(input)
 	if err != nil {
 		panic(err)
 	}
@@ -24,8 +30,19 @@ func main() {
 	fmt.Println(CalculateSimilarityScore(left, right))
 }
 
+func puzzle2() {
+	fmt.Println("Puzzle 2")
+	input, err := os.Open("./input/2.txt")
+	if err != nil {
+		panic(err)
+	}
+	reports, err := ParseInput2(input)
+	fmt.Println(CountSafeReports(reports))
+	fmt.Println(CountSafeReportsWithProblemDampener(reports))
+}
+
 // Puzzle 1
-func ParseInput(data io.Reader) (left, right []int, err error) {
+func ParseInput1(data io.Reader) (left, right []int, err error) {
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -78,4 +95,91 @@ func CalculateSimilarityScore(left, right []int) int {
 		score += val * rightCounts[val]
 	}
 	return score
+}
+
+// Puzzle 2
+func ParseInput2(data io.Reader) (reports [][]int, err error) {
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		line := scanner.Text()
+		tokens := strings.Split(line, " ")
+
+		report := make([]int, len(tokens))
+		for i, token := range tokens {
+			val, err := strconv.Atoi(token)
+			if err != nil {
+				return nil, err
+			}
+			report[i] = val
+		}
+		reports = append(reports, report)
+	}
+	return
+}
+
+func CountSafeReports(
+	reports [][]int) int {
+	numSafe := 0
+	for _, report := range reports {
+		if IsSafe(report, -1) {
+			numSafe++
+		}
+	}
+	return numSafe
+}
+
+func IsSafe(report []int, dropIndex int) bool {
+	hasIncrease := false
+	hasDecrease := false
+	prev := report[0]
+	if dropIndex == 0 {
+		prev = report[1]
+	}
+	firstRow := true
+	for index, val := range report {
+		if index == dropIndex {
+			continue
+		}
+		if firstRow {
+			firstRow = false
+			continue
+		}
+		if val > prev {
+			hasIncrease = true
+			diff := val - prev
+			if diff < 1 || diff > 3 {
+				return false
+			}
+		} else {
+			hasDecrease = true
+			diff := prev - val
+			if diff < 1 || diff > 3 {
+				return false
+			}
+		}
+		if hasIncrease && hasDecrease {
+			return false
+		}
+		prev = val
+	}
+	return true
+}
+
+func CountSafeReportsWithProblemDampener(reports [][]int) int {
+	numSafe := 0
+	for _, report := range reports {
+		if IsSafeWithProblemDampener(report) {
+			numSafe++
+		}
+	}
+	return numSafe
+}
+
+func IsSafeWithProblemDampener(report []int) bool {
+	for i := -1; i < len(report); i++ {
+		if IsSafe(report, i) {
+			return true
+		}
+	}
+	return false
 }
--- main_test.go
+++ main_test.go
@@ -10,8 +10,8 @@ import (
 var leftExample = []int{3, 4, 2, 1, 3, 3}
 var rightExample = []int{4, 3, 5, 3, 9, 3}
 
-func TestParseInput(t *testing.T) {
-	left, right, err := ParseInput(strings.NewReader(`3   4
+func TestParseInput1(t *testing.T) {
+	left, right, err := ParseInput1(strings.NewReader(`3   4
 4   3
 2   5
 1   3
@@ -32,6 +32,33 @@ func TestParseInput(t *testing.T) {
 	}
 }
 
+func TestParseInput2(t *testing.T) {
+	reports, err := ParseInput2(strings.NewReader(`7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+`))
+
+	expected := [][]int{
+		{7, 6, 4, 2, 1},
+		{1, 2, 7, 8, 9},
+		{9, 7, 6, 2, 1},
+		{1, 3, 2, 4, 5},
+		{8, 6, 4, 4, 1},
+		{1, 3, 6, 7, 9},
+	}
+
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	if !reflect.DeepEqual(reports, expected) {
+		t.Errorf("expected %v, got %v", expected, reports)
+	}
+}
+
 func TestDiffLocations(t *testing.T) {
 	distance := DiffLocations(slices.Clone(leftExample), slices.Clone(rightExample))
 	if distance != 11 {
@@ -43,5 +70,75 @@ func TestCalculateSimilarityScore(t *testing.T) {
 	score := CalculateSimilarityScore(slices.Clone(leftExample), slices.Clone(rightExample))
 	if score != 31 {
 		t.Errorf("expected 31, got %d", score)
+	}
+}
+
+func TestIsSafe(t *testing.T) {
+	examples := []struct {
+		report   []int
+		expected bool
+	}{
+		{[]int{7, 6, 4, 2, 1}, true},
+		{[]int{1, 2, 7, 8, 9}, false},
+		{[]int{9, 7, 6, 2, 1}, false},
+		{[]int{1, 3, 2, 4, 5}, false},
+		{[]int{8, 6, 4, 4, 1}, false},
+		{[]int{1, 3, 6, 7, 9}, true},
+	}
+
+	for _, example := range examples {
+		got := IsSafe(example.report, -1)
+		if got != example.expected {
+			t.Errorf("expected %v, got %v", example.expected, got)
+		}
+	}
+}
+
+func TestIsSafeDropIndex(t *testing.T) {
+	examples := []struct {
+		report    []int
+		dropIndex int
+		expected  bool
+	}{
+		{[]int{6, 1, 7}, -1, false},
+		{[]int{6, 1, 7}, 0, false},
+		{[]int{6, 1, 7}, 1, true},
+		{[]int{6, 1, 7}, 2, false},
+	}
+
+	for _, example := range examples {
+		got := IsSafe(example.report, example.dropIndex)
+		if got != example.expected {
+			t.Errorf("expected %v, got %v", example.expected, got)
+		}
+	}
+}
+
+func TestIsSafeWithProblemDampener(t *testing.T) {
+	examples := []struct {
+		report   []int
+		expected bool
+	}{
+
+		{[]int{7, 6, 4, 2, 1}, true},
+		{[]int{1, 2, 7, 8, 9}, false},
+		{[]int{9, 7, 6, 2, 1}, false},
+		{[]int{1, 3, 2, 4, 5}, true},
+		{[]int{8, 6, 4, 4, 1}, true},
+		{[]int{1, 3, 6, 7, 9}, true},
+		{[]int{1, 6, 7}, true},
+		{[]int{1, 7, 6}, true},
+		{[]int{6, 1, 7}, true},
+		{[]int{6, 7, 1}, true},
+		{[]int{7, 1, 6}, true},
+		{[]int{7, 6, 1}, true},
+	}
+
+	for _, example := range examples {
+		reportUnmutated := slices.Clone(example.report)
+		got := IsSafeWithProblemDampener(example.report)
+		if got != example.expected {
+			t.Errorf("%v: expected %v, got %v", reportUnmutated, example.expected, got)
+		}
 	}
 }
