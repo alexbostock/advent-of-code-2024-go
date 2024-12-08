1297cec84d8e6360e561f1d5903d454945f4c5e6
Alex Bostock <alex@bostock.uk>
2024-12-08 11:18:41 +0000

    Solve day 4

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 	puzzle1()
 	puzzle2()
 	puzzle3()
+	puzzle4()
 }
 
 func puzzle1() {
@@ -51,6 +52,17 @@ func puzzle3() {
 	commands := parseInput3(input)
 	fmt.Println(sumMuls(commands, false))
 	fmt.Println(sumMuls(commands, true))
+}
+
+func puzzle4() {
+	fmt.Println("Puzzle 4")
+	input, err := os.Open("./input/4.txt")
+	if err != nil {
+		panic(err)
+	}
+	wordSearch := ParseInput4(input)
+	fmt.Println(CountXMASInWordSearch(wordSearch))
+	fmt.Println(CountCrossMASInWordSearch(wordSearch))
 }
 
 // Puzzle 1
@@ -330,4 +342,95 @@ func sumMuls(commands []*Command, observeDosAndDonts bool) int {
 		}
 	}
 	return sum
+}
+
+// Puzzle 4
+type WordSearch struct {
+	chars  [][]rune
+	height int
+	width  int
+}
+
+func ParseInput4(data io.Reader) WordSearch {
+	var chars [][]rune
+	scanner := bufio.NewScanner(data)
+	height := 0
+	width := 0
+	for scanner.Scan() {
+		chars = append(chars, []rune(scanner.Text()))
+		height++
+		width = len(scanner.Text())
+	}
+	return WordSearch{
+		chars, height, width,
+	}
+}
+
+func CountXMASInWordSearch(wordSearch WordSearch) int {
+	count := 0
+	for i := 0; i < wordSearch.height; i++ {
+		for j := 0; j < wordSearch.width; j++ {
+			patterns := [][]struct {
+				i int
+				j int
+			}{
+				{{i, j}, {i + 1, j}, {i + 2, j}, {i + 3, j}},
+				{{i, j}, {i - 1, j}, {i - 2, j}, {i - 3, j}},
+				{{i, j}, {i, j + 1}, {i, j + 2}, {i, j + 3}},
+				{{i, j}, {i, j - 1}, {i, j - 2}, {i, j - 3}},
+				{{i, j}, {i + 1, j + 1}, {i + 2, j + 2}, {i + 3, j + 3}},
+				{{i, j}, {i + 1, j - 1}, {i + 2, j - 2}, {i + 3, j - 3}},
+				{{i, j}, {i - 1, j + 1}, {i - 2, j + 2}, {i - 3, j + 3}},
+				{{i, j}, {i - 1, j - 1}, {i - 2, j - 2}, {i - 3, j - 3}},
+			}
+			for _, pattern := range patterns {
+				if hasXMASInWordSearchAtPositions(wordSearch, pattern) {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
+func hasXMASInWordSearchAtPositions(wordSearch WordSearch, coords []struct {
+	i int
+	j int
+}) bool {
+	for pos, coord := range coords {
+		if coord.i < 0 || coord.i >= wordSearch.height || coord.j < 0 || coord.j >= wordSearch.width {
+			return false
+		}
+		expected := rune("XMAS"[pos])
+		if wordSearch.chars[coord.i][coord.j] != expected {
+			return false
+		}
+	}
+	return true
+}
+
+func CountCrossMASInWordSearch(wordSearch WordSearch) int {
+	count := 0
+	for i := 0; i < wordSearch.height; i++ {
+		for j := 0; j < wordSearch.width; j++ {
+			if hasCrossMASInWordSearchCentredAt(wordSearch, i, j) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func hasCrossMASInWordSearchCentredAt(wordSearch WordSearch, i, j int) bool {
+	if i-1 < 0 || i+1 >= wordSearch.height || j-1 < 0 || j+1 >= wordSearch.width {
+		return false
+	}
+	topLeft := wordSearch.chars[i-1][j-1]
+	topRight := wordSearch.chars[i-1][j+1]
+	bottomLeft := wordSearch.chars[i+1][j-1]
+	bottomRight := wordSearch.chars[i+1][j+1]
+	centre := wordSearch.chars[i][j]
+	diagonalOneOkay := topLeft == 'M' && bottomRight == 'S' || topLeft == 'S' && bottomRight == 'M'
+	diagonalTwoOkay := topRight == 'M' && bottomLeft == 'S' || topRight == 'S' && bottomLeft == 'M'
+	return centre == 'A' && diagonalOneOkay && diagonalTwoOkay
 }
