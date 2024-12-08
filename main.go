1e2cf78bebc5a3e5a4ed2cafe3d7e0af0e103bec
Alex Bostock <alex@bostock.uk>
2024-12-08 20:12:38 +0000

    Refactor main

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strconv"
 
 	"adventofcode2024/puzzle1"
 	"adventofcode2024/puzzle2"
@@ -15,22 +17,43 @@ import (
 )
 
 func main() {
-	solvePuzzle1()
-	solvePuzzle2()
-	solvePuzzle3()
-	solvePuzzle4()
-	solvePuzzle5()
-	solvePuzzle6()
-	solvePuzzle7()
-	solvePuzzle8()
+	puzzles := []struct {
+		puzzleNum int
+		solve     func(input io.Reader)
+	}{
+		{1, p1},
+		{2, p2},
+		{3, p3},
+		{4, p4},
+		{5, p5},
+		{6, p6},
+		{7, p7},
+		{8, p8},
+	}
+
+	puzzleNum := ""
+	if len(os.Args) > 1 {
+		puzzleNum = os.Args[1]
+	}
+
+	for _, puzzle := range puzzles {
+		if puzzleNum == "" || puzzleNum == strconv.Itoa(puzzle.puzzleNum) {
+			solvePuzzle(puzzle.puzzleNum, puzzle.solve)
+		}
+	}
 }
 
-func solvePuzzle1() {
-	fmt.Println("Puzzle 1")
-	input, err := os.Open("./input/1.txt")
+func solvePuzzle(puzzleNum int, solve func(input io.Reader)) {
+	fmt.Printf("Puzzle %v\n", puzzleNum)
+	input, err := os.Open(fmt.Sprintf("./input/%v.txt", puzzleNum))
 	if err != nil {
 		panic(err)
 	}
+	solve(input)
+	fmt.Println()
+}
+
+func p1(input io.Reader) {
 	left, right, err := puzzle1.ParseInput1(input)
 	if err != nil {
 		panic(err)
@@ -39,78 +62,46 @@ func solvePuzzle1() {
 	fmt.Println(puzzle1.CalculateSimilarityScore(left, right))
 }
 
-func solvePuzzle2() {
-	fmt.Println("Puzzle 2")
-	input, err := os.Open("./input/2.txt")
+func p2(input io.Reader) {
+	reports, err := puzzle2.ParseInput2(input)
 	if err != nil {
 		panic(err)
 	}
-	reports, err := puzzle2.ParseInput2(input)
 	fmt.Println(puzzle2.CountSafeReports(reports))
 	fmt.Println(puzzle2.CountSafeReportsWithProblemDampener(reports))
 }
 
-func solvePuzzle3() {
-	fmt.Println("Puzzle 3")
-	input, err := os.Open("./input/3.txt")
-	if err != nil {
-		panic(err)
-	}
+func p3(input io.Reader) {
 	commands := puzzle3.ParseInput3(input)
 	fmt.Println(puzzle3.SumMuls(commands, false))
 	fmt.Println(puzzle3.SumMuls(commands, true))
 }
 
-func solvePuzzle4() {
-	fmt.Println("Puzzle 4")
-	input, err := os.Open("./input/4.txt")
-	if err != nil {
-		panic(err)
-	}
+func p4(input io.Reader) {
 	wordSearch := puzzle4.ParseInput4(input)
 	fmt.Println(puzzle4.CountXMASInWordSearch(wordSearch))
 	fmt.Println(puzzle4.CountCrossMASInWordSearch(wordSearch))
 }
 
-func solvePuzzle5() {
-	fmt.Println("Puzzle 5")
-	inputData, err := os.Open("./input/5.txt")
-	if err != nil {
-		panic(err)
-	}
+func p5(inputData io.Reader) {
 	input := puzzle5.ParseInput5(inputData)
 	fmt.Println(puzzle5.SumMiddlePagesOfCorrectlyOrderedUpdates(input))
 	fmt.Println(puzzle5.SumMiddlePagesOfFixedUpdates(input))
 }
 
-func solvePuzzle6() {
-	fmt.Println("Puzzle 6")
-	input, err := os.Open("./input/6.txt")
-	if err != nil {
-		panic(err)
-	}
+func p6(input io.Reader) {
 	area := puzzle6.ParseInput6(input)
 	fmt.Println(puzzle6.CountGuardPositionsVisited(area.Clone()))
 	fmt.Println(puzzle6.CountPossibleNewObstaclesCausingLoops(area))
 }
 
-func solvePuzzle7() {
-	fmt.Println("Puzzle 7")
-	input, err := os.Open("./input/7.txt")
-	if err != nil {
-		panic(err)
-	}
+func p7(input io.Reader) {
 	equations := puzzle7.ParseInput7(input)
 	fmt.Println(puzzle7.SumValidCalibrationEquations(equations, false))
 	fmt.Println(puzzle7.SumValidCalibrationEquations(equations, true))
 }
 
-func solvePuzzle8() {
-	fmt.Println("Puzzle 8")
-	input, err := os.Open("./input/8.txt")
-	if err != nil {
-		panic(err)
-	}
+func p8(input io.Reader) {
 	area := puzzle8.ParseInput8(input)
 	fmt.Println(puzzle8.CountDistinctAntinodes(area))
 	fmt.Println(puzzle8.CountDistinctAntinodesWithResonantHarmonics(area))
