18790d0a9d63dfe7e5c7b9d97104997198cb090d
Alex Bostock <alex@bostock.uk>
2024-12-08 19:08:29 +0000

    Solve day 8

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	puzzle5()
 	puzzle6()
 	puzzle7()
+	puzzle8()
 }
 
 func puzzle1() {
@@ -109,6 +110,17 @@ func puzzle7() {
 	equations := ParseInput7(input)
 	fmt.Println(SumValidCalibrationEquations(equations, false))
 	fmt.Println(SumValidCalibrationEquations(equations, true))
+}
+
+func puzzle8() {
+	fmt.Println("Puzzle 8")
+	input, err := os.Open("./input/8.txt")
+	if err != nil {
+		panic(err)
+	}
+	area := ParseInput8(input)
+	fmt.Println(CountDistinctAntinodes(area))
+	fmt.Println(CountDistinctAntinodesWithResonantHarmonics(area))
 }
 
 // Puzzle 1
@@ -819,4 +831,89 @@ func smallestMultipleOfTenGreaterThan(num, multipleOfTen int) int {
 	} else {
 		return smallestMultipleOfTenGreaterThan(num, multipleOfTen*10)
 	}
+}
+
+// Puzzle 8
+type Puzzle8Input struct {
+	height   int
+	width    int
+	antennas map[rune][]Coords
+}
+
+func ParseInput8(data io.Reader) Puzzle8Input {
+	height := 0
+	width := 0
+	antennas := make(map[rune][]Coords)
+
+	scanner := bufio.NewScanner(data)
+	currentLineNum := -1
+	for scanner.Scan() {
+		currentLineNum++
+		height = currentLineNum + 1
+		line := scanner.Text()
+		width = len(line)
+		for indexInLine, char := range []rune(line) {
+			if char == '.' {
+				continue
+			}
+			antennas[char] = append(antennas[char], Coords{currentLineNum, indexInLine})
+		}
+	}
+
+	return Puzzle8Input{height, width, antennas}
+}
+
+func CountDistinctAntinodes(area Puzzle8Input) int {
+	antinodes := make(map[Coords]bool)
+	for _, nodes := range area.antennas {
+		for _, node1 := range nodes {
+			for _, node2 := range nodes {
+				if node1 == node2 {
+					continue
+				}
+
+				diffI := node1.i - node2.i
+				diffJ := node1.j - node2.j
+
+				antinode1 := Coords{node1.i + diffI, node1.j + diffJ}
+				antinode2 := Coords{node2.i - diffI, node2.j - diffJ}
+
+				if antinode1.i >= 0 && antinode1.i < area.height && antinode1.j >= 0 && antinode1.j < area.width {
+					antinodes[antinode1] = true
+				}
+				if antinode2.i >= 0 && antinode2.i < area.height && antinode2.j >= 0 && antinode2.j < area.width {
+					antinodes[antinode2] = true
+				}
+			}
+		}
+	}
+	return len(antinodes)
+}
+
+func CountDistinctAntinodesWithResonantHarmonics(area Puzzle8Input) int {
+	antinodes := make(map[Coords]bool)
+	for _, nodes := range area.antennas {
+		for _, antenna1 := range nodes {
+			for _, antenna2 := range nodes {
+				if antenna1 == antenna2 {
+					continue
+				}
+				gradient := float64(antenna1.i-antenna2.i) / float64(antenna1.j-antenna2.j)
+
+				for i := 0; i < area.height; i++ {
+					for j := 0; j < area.width; j++ {
+						pos := Coords{i, j}
+						if antinodes[pos] {
+							continue
+						}
+						gradientToAntenna1 := float64(antenna1.i-i) / float64(antenna1.j-j)
+						if gradientToAntenna1 == gradient {
+							antinodes[pos] = true
+						}
+					}
+				}
+			}
+		}
+	}
+	return len(antinodes)
 }
--- main_test.go
+++ main_test.go
@@ -562,7 +562,7 @@ func TestSumValidCalibrationEquations(t *testing.T) {
 		{292, []int{11, 6, 16, 20}},
 	}
 
-	got := SumValidCalibrationEquations(equations)
+	got := SumValidCalibrationEquations(equations, false)
 	if got != 3749 {
 		t.Errorf("expected 3749, got %v", got)
 	}
@@ -616,5 +616,64 @@ func TestCanSolveCalibrationEquationWithConcatenation(t *testing.T) {
 		if got != example.expected {
 			t.Errorf("%v: expected %v, got %v", example.equation, example.expected, got)
 		}
+	}
+}
+
+func TestParseInput8(t *testing.T) {
+	got := ParseInput8(strings.NewReader(`............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............
+`))
+
+	expected := Puzzle8Input{
+		height: 12,
+		width:  12,
+		antennas: map[rune][]Coords{
+			'0': {{1, 8}, {2, 5}, {3, 7}, {4, 4}},
+			'A': {{5, 6}, {8, 8}, {9, 9}},
+		},
+	}
+
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("expected %v, got %v", expected, got)
+	}
+}
+
+func TestCountDistinctAntinodes(t *testing.T) {
+	got := CountDistinctAntinodes(Puzzle8Input{
+		height: 12,
+		width:  12,
+		antennas: map[rune][]Coords{
+			'0': {{1, 8}, {2, 5}, {3, 7}, {4, 4}},
+			'A': {{5, 6}, {8, 8}, {9, 9}},
+		},
+	})
+
+	if got != 14 {
+		t.Errorf("expected 14, got %v", got)
+	}
+}
+
+func TestCountDistinctAntinodesWithResonantHarmonics(t *testing.T) {
+	got := CountDistinctAntinodesWithResonantHarmonics(Puzzle8Input{
+		height: 12,
+		width:  12,
+		antennas: map[rune][]Coords{
+			'0': {{1, 8}, {2, 5}, {3, 7}, {4, 4}},
+			'A': {{5, 6}, {8, 8}, {9, 9}},
+		},
+	})
+
+	if got != 34 {
+		t.Errorf("expected 34, got %v", got)
 	}
 }
